f3da4a3392ae5cdbf955ec7c876f8d1d4d25830e
opilane <opilane@HP-CZC6297B8P>
2021-10-28 11:34:53 +0300

    kamen,noshnizi,bumaga

--- Artjom Jurtjaev/Artjom_Jurtjaev.py
+++ Artjom Jurtjaev/Artjom_Jurtjaev.py
@@ -1,4 +1,26 @@
-print('Hello World')
-print('Hello World')
-print('Hello World')
-print('Hello World')
+from random import *
+print('Игра в камень, ножницы, бумага!')
+a=''
+b=0
+while a.isdigit()!=True:
+    a=input('Выберите камень=1, бумага=2, ножницы=3')
+try:
+    TypeError
+z=randint(1,3)
+if z==1:
+    print('Компьютер выбрал камень')
+elif z==2:
+    print('Компьютер выбрал бумага')
+else:
+    print('Компьютер выбрал ножницы')
+if a=z:
+    print('Ничья')
+elif a==1 and z==3:
+    print('Ты выйграл')
+    b+=1
+elif a<z:
+    print('Ты проиграл')
+    b-=1
+elif a>z:
+    print('Ты выйграл')
+
